194d2475283df6cc0617ef3fdc02286e2329f579
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-19 16:04:02 -0300

    default server

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from "express"
+import routes from "./routes"
+
+const app = express()
+routes(app)
+const port = 3000
+app.listen(port, () => console.log(`Server running at ${port}`))
