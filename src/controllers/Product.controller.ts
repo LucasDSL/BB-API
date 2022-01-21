8fe689341b10ffb5822cf47c941ad8f5daa90037
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-21 16:04:16 -0300

    Controller para rotas de products

---
+++ src/controllers/Product.controller.ts
@@ -0,0 +1,51 @@
+import express from "express"
+import { createConnection } from "typeorm"
+import { Products } from "../entity/Product.entity"
+import Errors from "../errors/listErrors"
+import newProduct from "../interfaces/newProduct"
+
+class ProductsController {
+  async allProducts(
+    req: express.Request,
+    res: express.Response,
+    next: Function
+  ) {
+    try {
+      const conn = await createConnection()
+      const allProducts = await conn.manager.find(Products)
+      if (allProducts.length === 0) {
+        await conn.close()
+        throw new Errors.ProdutoNaoEncotrado()
+      }
+      await conn.close()
+      return res.status(200).json(allProducts)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async addProduct(
+    req: express.Request,
+    res: express.Response,
+    next: Function
+  ) {
+    const productFromCliente: newProduct = req.body
+    try {
+      const fieldsAddProduct = ["name", "author", "price", "onStock"]
+      fieldsAddProduct.forEach((field) => {
+        if (!productFromCliente[field]) {
+          throw new Errors.CampoObrigatorio()
+        }
+      })
+
+      const conn = await createConnection()
+      const addedProduct = await conn.manager.insert(Products, productFromCliente)
+      await conn.close()
+      return res.status(200).end()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export default new ProductsController()
