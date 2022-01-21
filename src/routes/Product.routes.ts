dce6f6c9e7a318b1ee7101e510364e0f53bdfbff
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-21 16:03:54 -0300

    Rota para products

---
+++ src/routes/Product.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express"
+import { ProductsController } from "../controllers/listControllers"
+
+const router = Router()
+router.get("/products", ProductsController.allProducts)
+router.post("/products", ProductsController.addProduct)
+export default router 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import cors from "cors"
 import { Application, json } from "express"
 import CustomerRouter from "./Customer.routes"
+import ProductsRouter from "./Product.routes"
 import errorHandler from "../middlewares/errorHandler"
 
 export default (app: Application): void => {
   app.use(cors())
   app.use(json())
   app.use(CustomerRouter)
+  app.use(ProductsRouter)
   app.use(errorHandler)
 }
