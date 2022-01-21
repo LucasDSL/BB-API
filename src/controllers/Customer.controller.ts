42003ecbac75fc944753502f900a8aae5c8a34a6
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-21 12:48:34 -0300

    finalização das rotas de customers

--- src/controllers/Customer.controller.ts
+++ src/controllers/Customer.controller.ts
@@ -3,10 +3,12 @@ import newCustomer from "../interfaces/NewCustomer"
 import encryptPassword from "../middlewares/encryptPassword"
 import { createConnection } from "typeorm"
 import { Customers } from "../entity/Customer.entity"
-import { CampoObrigatorio, ClienteNaoEncontrado } from "../errors/listErrors"
+import Errors from "../errors/listErrors"
+import CampoInvalido from "../errors/CampoInvalido"
+import CampoObrigatorio from "../errors/CampoObrigatorio"
 
 class CustomerController {
-  static async createCustomer(
+  async createCustomer(
     req: express.Request,
     res: express.Response,
     next: Function
@@ -20,12 +22,13 @@ class CustomerController {
       "customerAddress",
     ]
     try {
+      console.log(this)
       fieldsNewCustomer.forEach((field) => {
         if (!customerFromClient[field]) {
-          throw new CampoObrigatorio()
+          throw new Errors.CampoObrigatorio()
         }
       })
-      
+
       customerFromClient.password = await encryptPassword(
         customerFromClient.password,
         next
@@ -48,29 +51,58 @@ class CustomerController {
     }
   }
 
-  static async updateCustomer(
+  async updateCustomer(
     req: express.Request,
     res: express.Response,
     next: Function
-  ) {}
+  ) {
+    const fieldsUpdate: string[] = [
+      "name",
+      "email",
+      "cellphone",
+      "password",
+      "customerAddress",
+    ]
 
-  static async deleteCustomer(
+    try {
+      const { customerId } = req.params
+      const updates = req.body
+      console.log(updates)
+      if (!updates) {
+        throw new CampoInvalido()
+      }
+      const control = new CustomerController()
+      control.isThereCostumer(Number(customerId), next)
+      const newPatch = Object.keys(updates)
+      newPatch.forEach((key) => {
+        if (fieldsUpdate.indexOf(key) === -1) {
+          throw new CampoInvalido(key)
+        }
+      })
+
+      const conn = await createConnection()
+      const update = await conn.manager.update(
+        Customers,
+        { id: customerId },
+        updates
+      )
+      conn.close()
+      return res.status(202).json(update)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteCustomer(
     req: express.Request,
     res: express.Response,
     next: Function
   ) {
     try {
       const { customerId } = req.params
+      const control = new CustomerController()
+      control.isThereCostumer(Number(customerId), next)
       const conn = await createConnection()
-      const isThereCustomer = await conn.manager.findOne(
-        Customers,
-        Number(customerId)
-      )
-      if (!isThereCustomer) {
-        await conn.close()
-        throw new ClienteNaoEncontrado(Number(customerId))
-      }
-
       await conn.manager.delete(Customers, {
         id: customerId,
       })
@@ -80,6 +112,22 @@ class CustomerController {
       next(error)
     }
   }
+
+  async isThereCostumer(id: number, next: Function) {
+    try {
+      const conn = await createConnection()
+      const isThereCostumer = await conn.manager.findOne(Customers, {
+        id: Number(id),
+      })
+      conn.close()
+      if (isThereCostumer) {
+        return
+      }
+      throw new Errors.ClienteNaoEncontrado(id)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
-export default CustomerController
+export default new CustomerController()
