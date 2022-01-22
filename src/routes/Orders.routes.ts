d18dbd6cf992db369e609a46d95826326e8902d7
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-22 13:25:42 -0300

    Finalização das rotas de orders(rotas principais da api) - faltam especiais

--- src/routes/Orders.routes.ts
+++ src/routes/Orders.routes.ts
@@ -3,5 +3,5 @@ import { OrderController } from "../controllers/listControllers"
 
 const router = Router()
 router.post("/orders", OrderController.addOrder)
-
+router.delete("/orders/:orderId", OrderController.deleteOrder)
 export default router 
