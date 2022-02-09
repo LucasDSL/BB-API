c9848dcaf02c9486972a9b984124276ae806b8e9
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-02-09 07:14:26 -0300

    rearranjo dos erros e middlewares para lidar com erros

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,21 +1,27 @@
-import Errors from "../shared/errors/listErrors"
+import CampoInvalido from "../shared/errors/CampoInvalido"
+import CampoObrigatorio from "../shared/errors/CampoObrigatorio"
+import ClienteNaoEncontrado from "../shared/errors/CllienteNaoEncontrado"
+import DadosIncorretos from "../shared/errors/DadosIncorretos"
+import PedidoNaoEncontrado from "../shared/errors/PedidoNaoEncontrado"
+import ProdutoNaoEncotrado from "../shared/errors/ProdutoNaoEncontrado"
+import QuantidadeInsuficiente from "../shared/errors/QuantidadeInsuficiente"
+
 export default (err, req, res, next) => {
   let status = 500
-  if (
-    err instanceof Errors.CampoObrigatorio ||
-    err instanceof Errors.CampoInvalido
-  ) {
+  if (err instanceof CampoObrigatorio || err instanceof CampoInvalido) {
     status = 400
   }
   if (
-    err instanceof Errors.ProdutoNaoEncotrado ||
-    err instanceof Errors.ClienteNaoEncontrado ||
-    err instanceof Errors.PedidoNaoEncontrado ||
-    err instanceof Errors.QuantidadeInsuficiente
+    err instanceof ProdutoNaoEncotrado ||
+    err instanceof ClienteNaoEncontrado ||
+    err instanceof PedidoNaoEncontrado ||
+    err instanceof QuantidadeInsuficiente
   ) {
     status = 404
   }
-
+  if (err instanceof DadosIncorretos) {
+    status = 401
+  }
   res.status(status).json({
     id: err.id,
     errorName: err.name,
