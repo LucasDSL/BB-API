944af9097016bcf28e2993b02e53c7aca3722dba
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-02-09 07:35:01 -0300

    Mudando erro de cliente não encontrado

--- src/shared/errors/CllienteNaoEncontrado.ts
+++ src/shared/errors/CllienteNaoEncontrado.ts
@@ -1,8 +1,8 @@
 export default class ClienteNaoEncontrado extends Error {
   id: number
   name: string
-  constructor(id: number = 0) {
-    super(`Cliente com id ${id} não encontrado!`)
+  constructor() {
+    super(`Cliente não encontrado!`)
     this.id = 2
     this.name = "ClienteNaoEncontrado"
   }
